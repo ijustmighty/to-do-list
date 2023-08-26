17ebbf9ac83bedf4f04580335a0f37fa9b657c6f
Slava Gapon <gapon.sv1026@gmail.com>
2023-08-26 21:36:15 +0300

    more js. change "ul" to "div"

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function AddTask() {
+function addTask() {
 	if (inputBox.value === '') {
 		alert("Ты должен написать свою задачу!");
 	}
 	else {
-		let li = document.createElement ("li");
-		li.innerHTML = inputBox.value;
-		listContainer.appendChild(li); 
-		let span = document.createElement ("span");
-		span.innerHTML = "\u00d7";
-		li.appendChild(span);
+		let blochok = document.createElement("div");
+		blochok.classList.add("tasks");
+		listContainer.appendChild(blochok);
+		const chekk = document.createElement("div");
+		chekk.classList.add("krugalochka");
+		blochok.appendChild(chekk);
+		let textik = document.createElement("p");
+		textik.innerHTML = inputBox.value;
+		blochok.appendChild(textik);
+		let pencil = document.createElement ("div");
+		pencil.classList.add ("editor");
+		blochok.appendChild(pencil);
+		let krest = document.createElement("span");
+		krest.innerHTML = "\u00d7";
+		blochok.appendChild(krest);
 	}
 	inputBox.value = "";
 	saveData();
 }
-
-listContainer.addEventListener("click", function(e) {
-	if(e.target.tagName === "LI") {
-		e.target.classList.toggle("checked")
-	}
-	else if(e.target.tagName === "SPAN"){
-		e.target.parentElement.remove();
-	}
-}, false);
 
 function saveData(){
 	localStorage.setItem("data", listContainer.innerHTML);
@@ -34,3 +34,21 @@ function showTask (){
 	listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask ();
+
+inputBox.addEventListener('keyup', function(event) {
+	if (event.key === 'Enter') {
+			addTask();
+	}
+});
+
+listContainer.addEventListener("click", function(e) {
+	if(e.target.tagName === "P") {
+		e.target.classList.toggle("checked");
+		chekk.classList.toggle("checked");
+	}
+	else if(e.target.tagName === "SPAN"){
+		e.target.parentElement.remove();
+	}
+}, false);
+	saveData();
+
